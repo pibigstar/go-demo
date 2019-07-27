d8773a182496121c4d0529f34413b827c626f810
pibigstar <741047261@qq.com>
2019-07-27 23:09:50 +0800

    remove the vendor

--- vendor/github.com/mnhkahn/maodou/README.md
+++
@@ -1,5 +0,0 @@
-# maodou
-A search engine written in golang.
-
-http://www.layoutit.com/build
-https://code.google.com/p/google-code-prettify/
--- vendor/github.com/xiocode/weigo/emotions.go
+++
@@ -1,6 +0,0 @@
-package weigo
-
-//通过id获取mid
-func (api *APIClient) get_statuses_querymid(params map[string]interface{}, result interface{}) error {
-	return api.GET("emotions", params, result)
-}
