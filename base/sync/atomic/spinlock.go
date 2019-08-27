4753465aabc90a7e37b1ae7ea47de423b4a05452
kw.lei <kw.lei@izhaohu.com>
2019-08-27 17:46:42 +0800

    feat(sync): go fmt the file

--- base/sync/atomic/spinlock.go
+++ base/sync/atomic/spinlock.go
@@ -9,7 +9,7 @@ import (
 func SpinLock() int32 {
 	var num int32 = 1
 	for {
-		if atomic.CompareAndSwapInt32(&num,1,0) {
+		if atomic.CompareAndSwapInt32(&num, 1, 0) {
 			break
 		}
 		time.Sleep(time.Microsecond * 100)
