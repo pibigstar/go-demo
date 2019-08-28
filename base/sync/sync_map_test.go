337d54fadc1413ae8ea75e4210cc8c54d5896a99
kw.lei <kw.lei@izhaohu.com>
2019-08-28 18:48:24 +0800

    feat(map): add sync map test

---
+++ base/sync/sync_map_test.go
@@ -0,0 +1,37 @@
+package sync_demo
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func TestSyncMap(t *testing.T) {
+	// 开箱即用
+	var users sync.Map
+
+	users.Store("pi", 1)
+	users.Store("big", 2)
+	users.Store("star", 3)
+
+	if u1, ok := users.Load("pi"); ok {
+		fmt.Println(u1)
+	}
+
+	// 如果存在则value为存在中的，如果不存在，则为66，并将其放入map
+	value, exist := users.LoadOrStore("hello", 66)
+	fmt.Println(value, exist)
+
+	users.Range(func(key, value interface{}) bool {
+		fmt.Println(key, value)
+		return true
+	})
+
+	users.Delete("pi")
+	fmt.Println("=======delete========")
+	users.Range(func(key, value interface{}) bool {
+		fmt.Println(key, value)
+		return true
+	})
+
+}
