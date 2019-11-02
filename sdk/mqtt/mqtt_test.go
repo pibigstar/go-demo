7625928adc6487d570a709266d6188e6e43c5fe8
pibigstar <741047261@qq.com>
2019-11-02 17:00:29 +0800

    feat(mqtt): only run test in windows

--- sdk/mqtt/mqtt_test.go
+++ sdk/mqtt/mqtt_test.go
@@ -3,46 +3,51 @@ package mqtt
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"sync"
 	"testing"
 	"time"
 )
 
 func TestMqtt(t *testing.T) {
-	var (
-		clientId = "pibigstar"
-		wg       sync.WaitGroup
-	)
-	client := NewClient(clientId)
-	err := client.Connect()
-	if err != nil {
-		t.Errorf(err.Error())
-	}
+	hostname, _ := os.Hostname()
+	fmt.Println(os.Hostname())
+	if hostname == "pibigstar" {
+		var (
+			clientId = hostname
+			wg       sync.WaitGroup
+		)
+		client := NewClient(clientId)
+		err := client.Connect()
+		if err != nil {
+			t.Errorf(err.Error())
+		}
 
-	wg.Add(1)
-	go func() {
-		err := client.Subscribe(func(c *Client, msg *Message) {
-			fmt.Printf("接收到消息: %+v \n", msg)
-			wg.Done()
-		}, 1, "mqtt")
+		wg.Add(1)
+		go func() {
+			err := client.Subscribe(func(c *Client, msg *Message) {
+				fmt.Printf("接收到消息: %+v \n", msg)
+				wg.Done()
+			}, 1, "mqtt")
 
+			if err != nil {
+				panic(err)
+			}
+		}()
+
+		msg := &Message{
+			ClientID: clientId,
+			Type:     "text",
+			Data:     "Hello Pibistar",
+			Time:     time.Now().Unix(),
+		}
+		data, _ := json.Marshal(msg)
+
+		err = client.Publish("mqtt", 1, false, data)
 		if err != nil {
 			panic(err)
 		}
-	}()
 
-	msg := &Message{
-		ClientID: clientId,
-		Type:     "text",
-		Data:     "Hello Pibistar",
-		Time:     time.Now().Unix(),
+		wg.Wait()
 	}
-	data, _ := json.Marshal(msg)
-
-	err = client.Publish("mqtt", 1, false, data)
-	if err != nil {
-		panic(err)
-	}
-
-	wg.Wait()
 }
