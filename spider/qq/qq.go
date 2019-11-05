6a46994224e7a941baf7c6a0a0a9a063a114dc34
kw.lei <kw.lei@izhaohu.com>
2019-11-05 15:00:10 +0800

    feat(qq): refactor the qq login

---
+++ spider/qq/qq.go
@@ -0,0 +1,62 @@
+package qq
+
+type User struct {
+	Title        string `json:"title"`
+	Account      string `json:"account"`
+	Nickname     string `json:"nickname"`
+	PtLocalToken string `json:"pt_local_token"`
+	Uin          string `json:"uin"`
+	Skey         string `json:"skey"`
+	PSkey        string `json:"p_skey"`
+	GTK          int    `json:"g_tk"`
+}
+
+const (
+	// qq空间
+	qzoneReferer   = "https://xui.ptlogin2.qq.com/cgi-bin/xlogin?proxy_url=https%3A//qzs.qq.com/qzone/v6/portal/proxy.html&daid=5&&hide_title_bar=1&low_login=0&qlogin_auto_login=1&no_verifyimg=1&link_target=blank&appid=549000912&style=22&target=self&s_url=https%3A%2F%2Fqzs.qzone.qq.com%2Fqzone%2Fv5%2Floginsucc.html%3Fpara%3Dizone&pt_qr_app=%E6%89%8B%E6%9C%BAQQ%E7%A9%BA%E9%97%B4&pt_qr_link=http%3A//z.qzone.com/download.html&self_regurl=https%3A//qzs.qq.com/qzone/v6/reg/index.html&pt_qr_help_link=http%3A//z.qzone.com/download.html&pt_no_auth=1"
+	qzoneTargetUrl = "https://qzs.qzone.qq.com/qzone/v5/loginsucc.html"
+
+	// qq好友
+	friendReferer   = "https://xui.ptlogin2.qq.com/cgi-bin/xlogin?appid=715030901&daid=73&hide_close_icon=1&pt_no_auth=1&s_url=https%3A%2F%2Fqun.qq.com%2Fmember.html%23gid%3D11186560"
+	friendTargetUrl = "https://qun.qq.com/member.html"
+)
+
+type QQType int
+
+const (
+	QQZone   QQType = 1
+	QQFriend QQType = 2
+)
+
+func (t QQType) Referer() string {
+	switch t {
+	case QQZone:
+		return qzoneReferer
+	case QQFriend:
+		return friendReferer
+	default:
+		return ""
+	}
+}
+
+func (t QQType) Title() string {
+	switch t {
+	case QQZone:
+		return "QZone"
+	case QQFriend:
+		return "Friends"
+	default:
+		return ""
+	}
+}
+
+func (t QQType) TargetUrl() string {
+	switch t {
+	case QQZone:
+		return qzoneTargetUrl
+	case QQFriend:
+		return friendTargetUrl
+	default:
+		return ""
+	}
+}
---
+++ spider/qq/qq_login_test.go
@@ -0,0 +1,24 @@
+package qq
+
+import (
+	"go-demo/utils/env"
+	"testing"
+)
+
+func TestQQLogin(t *testing.T) {
+	if env.IsCI() {
+		return
+	}
+
+	user, err := GetQQInfo(QQZone)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("%+v \n", user)
+
+	user, err = GetQQInfo(QQFriend)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("%+v \n", user)
+}
