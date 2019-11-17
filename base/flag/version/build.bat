4431adb18efa2e95e7a752802dd95ef9d63266e9
pibigstar <741047261@qq.com>
2019-11-17 16:27:35 +0800

    add build version demo

---
+++ base/flag/version/build.bat
@@ -0,0 +1,1 @@
+go build -ldflags "-X 'main.goVersion=$(go version)' -X 'main.gitHash=$(git show -s --format=%H)' -X 'main.buildTime=$(git show -s --format=%cd)'" -o main.exe version.go
---
+++ base/flag/version/version.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var (
+	gitHash   string
+	buildTime string
+	goVersion string
+)
+
+func main() {
+	args := os.Args
+	if len(args) == 2 && (args[1] == "--version" || args[1] == "-v") {
+		fmt.Printf("Git Commit Hash: %s \n", gitHash)
+		fmt.Printf("Build TimeStamp: %s \n", buildTime)
+		fmt.Printf("GoLang Version: %s \n", goVersion)
+		return
+	}
+}
