ca86aa495e25a0e9934562d3aba565465e152776
leikewei <leikewei@theduapp.com>
2020-05-12 10:29:14 +0800

    add regexp

--- base/regexp/regexp_test.go
+++ base/regexp/regexp_test.go
@@ -1,6 +1,7 @@
 package regexp
 
 import (
+	"fmt"
 	"regexp"
 	"testing"
 )
@@ -9,9 +10,8 @@ import (
 func TestRegexp(t *testing.T) {
 	// 手机号检查
 	r := regexp.MustCompile("(13|14|15|17|18|19)[0-9]{9}")
-	find := r.Find([]byte("13838254613"))
-
-	t.Log(string(find))
+	find := r.MatchString("13838254613")
+	t.Log(find)
 
 	// 正则替换
 	// #后添加空格
@@ -22,6 +22,16 @@ func TestRegexp(t *testing.T) {
 		s = s + " "
 		return s
 	})
-
 	t.Log(newStr)
+
+	// 分组查询, 判断src里面链接是否为URL
+	s := "<img src='https://www.baidu.com' />"
+	re := regexp.MustCompile(`<img src=['|"]https.*\.(.*)['|"].*\/>`)
+	// 第一个是匹配字符串本身，后面的是正则()里面的内容
+	params := re.FindStringSubmatch(s)
+	if len(params) > 1 {
+		if params[1] != "jpg" {
+			fmt.Println(params[1])
+		}
+	}
 }
