719424539ed8ebd7e292a245a1ab372435638f3a
leikewei <leikewei@theduapp.com>
2020-05-13 20:05:39 +0800

    add hystrix

--- utils/hystrix/hystrix_test.go
+++ utils/hystrix/hystrix_test.go
@@ -11,10 +11,11 @@ import (
 
 func TestHystrix(t *testing.T) {
 	hystrix.ConfigureCommand("testHystrix", hystrix.CommandConfig{
-		// 10s
-		Timeout:               10000,
-		MaxConcurrentRequests: 100,
-		ErrorPercentThreshold: 25,
+		Timeout:                1000, // 单次请求 超时时间
+		MaxConcurrentRequests:  1,    // 最大并发量
+		RequestVolumeThreshold: 1,    // 10秒内如果出现1次错误就触发熔断
+		ErrorPercentThreshold:  1,    // 按百分比，如果出现1%的错误就触发熔断
+		SleepWindow:            5000, // 熔断后5秒后再去尝试服务是否可用
 	})
 
 	GetInfo()
@@ -26,7 +27,7 @@ func GetInfo() {
 	hystrix.Go("testHystrix", func() error {
 		// 业务逻辑部分
 		fmt.Println("处理业务逻辑")
-		// 模拟耗时操作，如果超过 配置的 10s,将会进入降级处理
+		// 模拟耗时操作，如果超过 配置的 1s,将会进入降级处理
 		time.Sleep(1 * time.Second)
 		return errors.New("i am dead")
 	}, func(err error) error {
