fe301f3bd731005b28b3c5a4129716e49b1c054b
leikewei <leikewei@theduapp.com>
2020-05-14 09:37:01 +0800

    add hystrix test

--- utils/hystrix/hystrix_test.go
+++ utils/hystrix/hystrix_test.go
@@ -3,36 +3,41 @@ package hystrix
 import (
 	"errors"
 	"fmt"
+	"github.com/afex/hystrix-go/hystrix"
 	"testing"
 	"time"
-
-	"github.com/afex/hystrix-go/hystrix"
 )
 
 func TestHystrix(t *testing.T) {
-	hystrix.ConfigureCommand("testHystrix", hystrix.CommandConfig{
-		Timeout:                1000, // 单次请求 超时时间
+	hystrix.ConfigureCommand("test", hystrix.CommandConfig{
 		MaxConcurrentRequests:  1,    // 最大并发量
+		Timeout:                3000, // 单次请求超时时间，如果超过3s将认为服务不可用
 		RequestVolumeThreshold: 1,    // 10秒内如果出现1次错误就触发熔断
 		ErrorPercentThreshold:  1,    // 按百分比，如果出现1%的错误就触发熔断
-		SleepWindow:            5000, // 熔断后5秒后再去尝试服务是否可用
+		SleepWindow:            2000, // 熔断后2秒后再去尝试服务是否可用
 	})
 
-	GetInfo()
-
-	time.Sleep(3 * time.Second)
+	for i := 0; i < 10; i++ {
+		GetInfo(i)
+		time.Sleep(1 * time.Second)
+	}
 }
 
-func GetInfo() {
-	hystrix.Go("testHystrix", func() error {
+func GetInfo(i int) {
+	hystrix.Go("test", func() error {
 		// 业务逻辑部分
-		fmt.Println("处理业务逻辑")
-		// 模拟耗时操作，如果超过 配置的 1s,将会进入降级处理
-		time.Sleep(1 * time.Second)
-		return errors.New("i am dead")
+		fmt.Println("处理业务逻辑...")
+		if i%2 == 0 {
+			return errors.New("this is error")
+		}
+		return nil
 	}, func(err error) error {
 		// 业务逻辑处理失败, 降级处理
-		fmt.Printf("调用失败, 开始降级处理: %s \n", err.Error())
+		if err == hystrix.ErrCircuitOpen {
+			fmt.Println("服务处于熔断中...")
+		} else {
+			fmt.Println("服务处理失败, err: ", err.Error())
+		}
 		return err
 	})
 }
