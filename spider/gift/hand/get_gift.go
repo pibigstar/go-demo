a072c89018d84737d14afbfcd7b5bc73694d2940
leikewei <leikewei@theduapp.com>
2020-07-14 09:55:15 +0800

    modify gift spider

--- spider/gift/hand/get_gift.go
+++ spider/gift/hand/get_gift.go
@@ -10,11 +10,16 @@ import (
 )
 
 type Gift struct {
-	Name string `json:"name"`
+	Name   string `json:"name"`
+	LuckId int    `json:"luckid"`
 }
 
-// 尝试次数
-const TryTime = 3
+const (
+	// 尝试次数
+	TryTime = 3
+	// 请求地址
+	URL = "https://pay.qun.qq.com/cgi-bin/group_pay/good_feeds/draw_lucky_gift"
+)
 
 //手动
 func main() {
@@ -22,25 +27,37 @@ func main() {
 	date := now.Format("2006-01-02")
 	for i := 0; i < TryTime; i++ {
 		client := &http.Client{}
-		req, err := http.NewRequest("POST", "https://pay.qun.qq.com/cgi-bin/group_pay/good_feeds/draw_lucky_gift", strings.NewReader("bkn=183506344&from=0&gc=40636692&client=1&version=7.7.0.3645"))
+		req, err := http.NewRequest("POST", URL, strings.NewReader("bkn=1858570791&from=0&gc=594174090&client=1&version=0"))
+		if err != nil {
+			return
+		}
 		req.Header.Set("Content-Type", "application/json;charset=utf-8")
 		req.Header.Set("Cookie", getCookie())
 
 		resp, err := client.Do(req)
+		if err != nil {
+			return
+		}
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			fmt.Printf("%s 请求内容失败,第%d次尝试 \n", date, i+1)
 			continue
 		}
 		defer resp.Body.Close()
-		var gift = Gift{}
+
+		if strings.Contains(string(body), "basekey") {
+			fmt.Printf("%s Cookie已失效,第%d次尝试 \n", date, i+1)
+			break
+		}
+
+		var gift Gift
 		err = json.Unmarshal(body, &gift)
 		if err != nil {
 			fmt.Printf("%s JSON解析失败,第%d次尝试 \n", date, i+1)
 			continue
 		}
-		if gift.Name == "" || len(gift.Name) == 0 {
-			fmt.Printf("%s Cookie已失效,第%d次尝试 \n", date, i+1)
+		if gift.Name == "" || gift.LuckId == 0 {
+			fmt.Println(date + " 未获得礼物")
 			break
 		}
 		fmt.Println(date + " 获得礼物:" + gift.Name)
@@ -49,7 +66,7 @@ func main() {
 }
 
 func getCookie() string {
-	bytes, err := ioutil.ReadFile("http/gift/cookie")
+	bytes, err := ioutil.ReadFile("spider/gift/cookie")
 	if err != nil {
 		fmt.Println("读取Cookie失败:", err.Error())
 	}
