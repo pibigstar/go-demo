cafcc495096e2291289b137df41d6b4c8d9e5a85
leikewei <leikewei@theduapp.com>
2020-12-14 17:45:40 +0800

    add stack

--- utils/stack/stack.go
+++ utils/stack/stack.go
@@ -1,7 +1,10 @@
 package stack
 
 import (
+	"fmt"
+	"path/filepath"
 	"runtime"
+	"runtime/debug"
 )
 
 // 获取函数调用栈
@@ -9,4 +12,27 @@ func GetStack() string {
 	var buf [4096]byte
 	n := runtime.Stack(buf[:], false)
 	return string(buf[:n])
+}
+
+// 获取函数调用栈
+func GetStackSimple() string {
+	stack := debug.Stack()
+	return string(stack)
+}
+
+// 获取函数调用栈
+func GetStackFunc() string {
+	pc, file, line, ok := runtime.Caller(2)
+	if !ok {
+		return ""
+	}
+	funcName := runtime.FuncForPC(pc).Name()
+	return fmt.Sprintf("文件: %s, func: %s, 行号: %d", file, funcName, line)
+}
+
+// 获取当前go文件的绝对路径
+func GetGoFilePath() string {
+	_, currentFile, _, _ := runtime.Caller(0)
+	basePath := filepath.Dir(currentFile)
+	return basePath
 }
