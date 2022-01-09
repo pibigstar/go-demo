42742690b770cb8af1baeede6df4d08736fd0256
雷科伟 <leikewei@shizhuang-inc.com>
2022-01-09 11:57:33 +0800

    add leetcode

--- leetcode/18/18.go
+++ leetcode/18/18.go
@@ -10,7 +10,27 @@ func PreorderByRecursion(root *tree.TreeNode) {
 	if root == nil {
 		return
 	}
-	fmt.Println(root.Val)
+	fmt.Println(root.Val)           // 前序：第一次在栈顶时就打印
 	PreorderByRecursion(root.Left)  // 将左节点入栈
 	PreorderByRecursion(root.Right) // 将右节点入栈
+}
+
+// 中序遍历-递归
+func InorderByRecursion(root *tree.TreeNode) {
+	if root == nil {
+		return
+	}
+	InorderByRecursion(root.Left)  // 将左节点入栈
+	fmt.Println(root.Val)          // 中序：第二次在栈顶时就打印
+	InorderByRecursion(root.Right) // 将右节点入栈
+}
+
+// 后序遍历-递归
+func AfterOderByRecursion(root *tree.TreeNode) {
+	if root == nil {
+		return
+	}
+	AfterOderByRecursion(root.Left)  // 将左节点入栈
+	AfterOderByRecursion(root.Right) // 将右节点入栈
+	fmt.Println(root.Val)            // 后序：第三次在栈顶时就打印
 }
--- leetcode/18/18_test.go
+++ leetcode/18/18_test.go
@@ -17,3 +17,15 @@ func TestPreorderByRecursion(t *testing.T) {
 	root := tree.GetTree()
 	PreorderByRecursion(root)
 }
+
+// 中序遍历-递归
+func TestInorderByRecursion(t *testing.T) {
+	root := tree.GetTree()
+	InorderByRecursion(root)
+}
+
+// 后序遍历-递归
+func TestAfterOderByRecursion(t *testing.T) {
+	root := tree.GetTree()
+	AfterOderByRecursion(root)
+}
