2f86e17bd74c98deb110885e46dc2364fa097ae5
雷科伟 <leikewei@shizhuang-inc.com>
2022-02-13 16:21:08 +0800

    add runtime thread

--- base/runtime/runtime_test.go
+++ base/runtime/runtime_test.go
@@ -2,7 +2,10 @@ package runtime
 
 import (
 	"fmt"
+	"os/exec"
 	"runtime"
+	"runtime/debug"
+	"runtime/pprof"
 	"testing"
 	"time"
 )
@@ -25,4 +28,40 @@ func demo1() {
 	// 退出goroutine
 	runtime.Goexit()
 	fmt.Println("不会执行了")
+}
+
+func TestSetMaxThread(t *testing.T) {
+	// 设置最大线程数
+	// 超过这个线程数之后程序会支付fatal
+	debug.SetMaxThreads(10)
+	for i := 0; i < 2; i++ {
+		go func() {
+			// 通过执行 shell 命令 sleep 3 来模拟同步系统调用过程
+			_, err := exec.Command("bash", "-c", "sleep 1").Output()
+			if err != nil {
+				panic(err)
+			}
+		}()
+	}
+	time.Sleep(time.Second)
+}
+
+// 通过runtime.LockOSThread()将M与G进行绑定
+// 在不执行runtime.UnlockOSThread的情况下
+// 当G退出后，空闲的M也会退出，从而达到释放空闲线程的作用
+func TestLockOSThread(t *testing.T) {
+	threadProfile := pprof.Lookup("threadcreate")
+	t.Logf("初始化时线程数： %d\n", threadProfile.Count())
+
+	for i := 0; i < 3; i++ {
+		go func() {
+			runtime.LockOSThread()
+			_, err := exec.Command("bash", "-c", "sleep 1").Output()
+			if err != nil {
+				panic(err)
+			}
+		}()
+	}
+	time.Sleep(time.Second)
+	t.Logf("结束时线程数: %d\n", threadProfile.Count())
 }
